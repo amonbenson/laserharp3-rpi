bd5d4137b4bc04b64643b412008877ac30b1076e
Amon Benson <amon.k.benson@campus.tu-berlin.de>
2023-05-19 22:00:26 +0200

    updates

--- laserharp/__init__.py
+++ laserharp/__init__.py
@@ -2,16 +2,37 @@ import cv2
 
 
 # initialize camera
-vid = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
+
+# set camera properties
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+cap.set(cv2.CAP_PROP_FPS, 60)
+
+# manual exposure
+cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
+cap.set(cv2.CAP_PROP_EXPOSURE, 50)
+
+# manual focus to infinity
+cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
+cap.set(cv2.CAP_PROP_FOCUS, 0)
+
+i = 0
+
 
 # main loop
 while True:
-    ret, frame = vid.read()
-    cv2.imshow('frame', frame)
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    mask = cv2.inRange(gray, 150, 255)
+
+    if i % 3 == 0:
+        cv2.imshow('frame', mask)
+    i += 1
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # cleanup
-vid.release()
+cap.release()
 cv2.destroyAllWindows()
